fb9414738c335235286b46d900dd45732354e0eb
sivert <sivert.b.heisholt@protonmail.com>
2021-10-14 15:07:37 +0200

    Vi har bilde

--- src/com/company/scenes/TestScene.java
+++ src/com/company/scenes/TestScene.java
@@ -1,10 +1,18 @@
 package com.company.scenes;
 
+import com.company.Main;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class TestScene {
 
     public TestScene(Stage stage) {
+        Pane pane = new Pane();
 
+        Scene scene = new Scene(pane, Main.WINDOW_WIDTH, Main.WINDOW_HEIGHT);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
